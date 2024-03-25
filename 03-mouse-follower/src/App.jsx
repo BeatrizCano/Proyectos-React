22161ab71ae19ffa8ff94f8bfcf875db46b12f88
Beatriz Cano <beatrizcafernandez@gmail.com>
2024-03-25 12:23:03 +0100

    create_03_project_and_add_eslint

---
+++ 03-mouse-follower/src/App.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  //pointer move
+  useEffect (() => {
+   console.log('effect', { enabled })
+
+   const handleMove = (event) => {
+    const { clientX, clientY } = event
+    console.log('handleMove', { clientX, clientY })
+    setPosition({ x: clientX, y: clientY})
+   }
+
+   if (enabled) {
+    window.addEventListener('pointermove', handleMove)
+   }
+
+   return () => {
+    window.removeEventListener('pointermove', handleMove)
+   }
+   
+  }, [enabled])
+
+  //useEffect:
+  //[] -> solo se ejecuta una vez cuando se monta el componente
+  //[enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  //undefined -> se ejecuta cada vez que se renderiza el componente
+
+
+  //change body classname
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', !enabled)
+
+    return () => { //cleanup method
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }} 
+      />    
+      <button onClick={() => setEnabled(!enabled)}> 
+        {enabled? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+function App() { 
+ 
+  return (
+    <main>
+       <FollowMouse />
+    </main>
+  
+  )
+}
+
+export default App
